82852ccae1f18dc14e4adf5f99f25255a0373bf7
Andrii <andriychikulay@gmail.com>
2022-04-28 10:09:01 +0300

    Updates

---
+++ main.40b6e51f63ed71e5e057.js
@@ -0,0 +1,316 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app.js":
+/*!****************!*\
+  !*** ./app.js ***!
+  \****************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/main.css */ \"./css/main.css\");\n/* harmony import */ var normalize_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! normalize.css */ \"../node_modules/normalize.css/normalize.css\");\n/* harmony import */ var _babel__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./babel */ \"./babel.js\");\n/* harmony import */ var _babel__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_babel__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _images_paint1_jpeg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./images/paint1.jpeg */ \"./images/paint1.jpeg\");\n/* harmony import */ var _images_paint2_jpeg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./images/paint2.jpeg */ \"./images/paint2.jpeg\");\n/* harmony import */ var _images_paint3_jpeg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./images/paint3.jpeg */ \"./images/paint3.jpeg\");\n/* harmony import */ var _images_paint4_jpeg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./images/paint4.jpeg */ \"./images/paint4.jpeg\");\n/* harmony import */ var _images_paint5_jpeg__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./images/paint5.jpeg */ \"./images/paint5.jpeg\");\n/* harmony import */ var _images_paint6_jpeg__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./images/paint6.jpeg */ \"./images/paint6.jpeg\");\n/* harmony import */ var _images_image2_jpeg__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./images/image2.jpeg */ \"./images/image2.jpeg\");\n/* harmony import */ var _images_img3_jpeg__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./images/img3.jpeg */ \"./images/img3.jpeg\");\n/* harmony import */ var _images_logo_png__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./images/logo.png */ \"./images/logo.png\");\n/* harmony import */ var _images_colorif_png__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./images/colorif.png */ \"./images/colorif.png\");\n/* harmony import */ var _images_slogan_png__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! ./images/slogan.png */ \"./images/slogan.png\");\n/* harmony import */ var _images_quote_svg__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! ./images/quote.svg */ \"./images/quote.svg\");\n/* harmony import */ var _images_insta_svg__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! ./images/insta.svg */ \"./images/insta.svg\");\n/* harmony import */ var _images_facebook_svg__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ./images/facebook.svg */ \"./images/facebook.svg\");\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nvar navToggle = document.querySelector(\".navbar_toggle\");\nvar links = document.querySelector(\".main_nav\");\nnavToggle.addEventListener('click', function () {\n  links.classList.toggle(\"show_nav\");\n});\n\n//# sourceURL=webpack:///./app.js?");
+
+/***/ }),
+
+/***/ "./babel.js":
+/*!******************!*\
+  !*** ./babel.js ***!
+  \******************/
+/***/ (function() {
+
+eval("function initMap() {\n  // The location of Kalush\n  var Kalush = {\n    lat: 49.0445757,\n    lng: 23.2412288\n  }; // The map, centered at Kalush\n\n  var map = new google.maps.Map(document.getElementById(\"map\"), {\n    zoom: 7,\n    center: Kalush\n  }); // The marker, positioned at Kalush\n\n  var marker = new google.maps.Marker({\n    position: Kalush,\n    map: map\n  });\n}\n\nwindow.initMap = initMap;\n\n//# sourceURL=webpack:///./babel.js?");
+
+/***/ }),
+
+/***/ "./images/colorif.png":
+/*!****************************!*\
+  !*** ./images/colorif.png ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"1c24a6a823934394defda4f5606c38f6.png\");\n\n//# sourceURL=webpack:///./images/colorif.png?");
+
+/***/ }),
+
+/***/ "./images/facebook.svg":
+/*!*****************************!*\
+  !*** ./images/facebook.svg ***!
+  \*****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"145b70d3ebc4c999f7ddfbb7449fa5f9.svg\");\n\n//# sourceURL=webpack:///./images/facebook.svg?");
+
+/***/ }),
+
+/***/ "./images/image2.jpeg":
+/*!****************************!*\
+  !*** ./images/image2.jpeg ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"06eecc64808260d704820e3d57e69949.jpeg\");\n\n//# sourceURL=webpack:///./images/image2.jpeg?");
+
+/***/ }),
+
+/***/ "./images/img3.jpeg":
+/*!**************************!*\
+  !*** ./images/img3.jpeg ***!
+  \**************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"858a0067c8d481360ae209f3dbbf57b5.jpeg\");\n\n//# sourceURL=webpack:///./images/img3.jpeg?");
+
+/***/ }),
+
+/***/ "./images/insta.svg":
+/*!**************************!*\
+  !*** ./images/insta.svg ***!
+  \**************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"b0c8109439165d014a2c44af906fd089.svg\");\n\n//# sourceURL=webpack:///./images/insta.svg?");
+
+/***/ }),
+
+/***/ "./images/logo.png":
+/*!*************************!*\
+  !*** ./images/logo.png ***!
+  \*************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"88669df08c499431fbedf1c50837cfd7.png\");\n\n//# sourceURL=webpack:///./images/logo.png?");
+
+/***/ }),
+
+/***/ "./images/paint1.jpeg":
+/*!****************************!*\
+  !*** ./images/paint1.jpeg ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"8b32b9384df27cbf88fea8faac9d8e67.jpeg\");\n\n//# sourceURL=webpack:///./images/paint1.jpeg?");
+
+/***/ }),
+
+/***/ "./images/paint2.jpeg":
+/*!****************************!*\
+  !*** ./images/paint2.jpeg ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"3605d547475fe90178aabf1408cb65f6.jpeg\");\n\n//# sourceURL=webpack:///./images/paint2.jpeg?");
+
+/***/ }),
+
+/***/ "./images/paint3.jpeg":
+/*!****************************!*\
+  !*** ./images/paint3.jpeg ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"d3d6a3952060a518ca32d995894ef520.jpeg\");\n\n//# sourceURL=webpack:///./images/paint3.jpeg?");
+
+/***/ }),
+
+/***/ "./images/paint4.jpeg":
+/*!****************************!*\
+  !*** ./images/paint4.jpeg ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"727238424bc5e7a96b2d901833d6dd52.jpeg\");\n\n//# sourceURL=webpack:///./images/paint4.jpeg?");
+
+/***/ }),
+
+/***/ "./images/paint5.jpeg":
+/*!****************************!*\
+  !*** ./images/paint5.jpeg ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"77d878e0f33679abbebccf49f8b76000.jpeg\");\n\n//# sourceURL=webpack:///./images/paint5.jpeg?");
+
+/***/ }),
+
+/***/ "./images/paint6.jpeg":
+/*!****************************!*\
+  !*** ./images/paint6.jpeg ***!
+  \****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"4f19b7bcfa95ea9c2549aa59145192a0.jpeg\");\n\n//# sourceURL=webpack:///./images/paint6.jpeg?");
+
+/***/ }),
+
+/***/ "./images/quote.svg":
+/*!**************************!*\
+  !*** ./images/quote.svg ***!
+  \**************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"ec6ef70cd85b8245428426d7ca38a591.svg\");\n\n//# sourceURL=webpack:///./images/quote.svg?");
+
+/***/ }),
+
+/***/ "./images/slogan.png":
+/*!***************************!*\
+  !*** ./images/slogan.png ***!
+  \***************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"595ae20cdf3f537c2da2d227fa16de6a.png\");\n\n//# sourceURL=webpack:///./images/slogan.png?");
+
+/***/ }),
+
+/***/ "../node_modules/normalize.css/normalize.css":
+/*!***************************************************!*\
+  !*** ../node_modules/normalize.css/normalize.css ***!
+  \***************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///../node_modules/normalize.css/normalize.css?");
+
+/***/ }),
+
+/***/ "./css/main.css":
+/*!**********************!*\
+  !*** ./css/main.css ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/main.css?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	!function() {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	!function() {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./app.js");
+/******/ 	
+/******/ })()
+;

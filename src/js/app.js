51a76a2c03ce5a2a672e6701d4078ef9539d6c5c
Andrii <andriychikulay@gmail.com>
2022-05-01 12:45:58 +0300

    no-scroll on menu

--- src/js/app.js
+++ src/js/app.js
@@ -7,11 +7,40 @@ import '../../node_modules/photoswipe/dist/photoswipe.css';
 import './babel'
 import './photoswipe'
 
-const navToggle = document.querySelector(".navbar_toggle");
-const links = document.querySelector(".main_nav");
+const navToggle = document.querySelector(".navbar_toggle")
+const links = document.querySelector(".main_nav")
+const body = document.querySelector('body')
+const toggle = document.querySelector('show_nav')
 
-navToggle.addEventListener('click', function(){
-    links.classList.toggle("show_nav");
+
+navToggle.addEventListener('click', () => {
+    if(links.classList.toggle("show_nav")){
+        window.onscroll =  function (){
+            window.scrollTo(0, 0);
+        }
+    }else {
+        window.onscroll = () => {
+            window.scroll()
+        }
+    }
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

f675c8f25b82cd0a9e5e6ee90d17fd299860e028
Andrii <andriychikulay@gmail.com>
2022-05-05 12:05:11 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -7,32 +7,6 @@ import '../../node_modules/photoswipe/dist/photoswipe.css';
 import './babel'
 import './photoswipe'
 
-const navToggle = document.querySelector(".navbar_toggle")
-const links = document.querySelector(".main_nav")
-const navbar = document.querySelector('.navbar')
-// const body = document.querySelector('body')
-
-
-navToggle.addEventListener('click', () => {
-    if(links.classList.toggle("show_nav")){
-        window.onscroll =  function (){
-            window.scrollTo(0, 0);
-        }
-    }else {
-        window.onscroll = () => {
-            window.scroll()
-        }
-    }
-})
-
-
-window.addEventListener('scroll', function(){
-    if(window.scrollY > 30) {
-        navbar.style.position = 'fixed'
-    }else {
-        navbar.style.position = ''
-    }
-})
 
 
 
--- src/js/babel.js
+++ src/js/babel.js
@@ -1,3 +1,8 @@
+const navToggle = document.querySelector(".navbar_toggle")
+const links = document.querySelector(".main_nav")
+const navbar = document.querySelector('.navbar')
+
+
 function initMap() {
     // The location of Kalush
     const Kalush = { lat: 48.932396, lng: 24.7124942 };
@@ -14,3 +19,34 @@ function initMap() {
     }
     
 window.initMap = initMap;
+
+
+navToggle.addEventListener('click', () => {
+    if(links.classList.toggle("show_nav")){
+        window.onscroll =  function (){
+            window.scrollTo(0, 0);
+        }
+    }else {
+        window.onscroll = () => {
+            window.scroll()
+        }
+    }
+})
+
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 30) {
+        navbar.style.position = 'fixed'
+    }else {
+        navbar.style.position = ''
+    }
+})
+
+
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+
+
